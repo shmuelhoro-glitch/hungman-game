fe22cd78f3d5334455f5a61488334b43076d91dd
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-05-12 20:08:41 +0300

    Adding the diagram and program file with basic headings

---
+++ game.py
@@ -0,0 +1,10 @@
+
+# הגדרת תנאי המשחק
+list_of_words_easy = ["apple", "water", "house", "smile", "bread"]
+list_of_words_medium = ["python", "puzzle", "guitar", "castle", "planet"]
+list_of_words_hard = ["developer", "algorithm", "xylophone", "cryptography", "jazz"]
+
+NUM_CHANCE = 10
+
+def main_menu():
+    print()
