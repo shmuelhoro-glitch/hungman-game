0bf0fe269186e3f83c42e01c13b70587a7d4c8cc
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-05-13 11:30:39 +0300

    Added implementation for functions to print menu, get name and level, and get random word

--- game.py
+++ game.py
@@ -1,10 +1,87 @@
-
+import random
 # הגדרת תנאי המשחק
-list_of_words_easy = ["apple", "water", "house", "smile", "bread"]
-list_of_words_medium = ["python", "puzzle", "guitar", "castle", "planet"]
-list_of_words_hard = ["developer", "algorithm", "xylophone", "cryptography", "jazz"]
+level_1 = ["apple", "water", "house", "smile", "bread"]
+level_2 = ["python", "puzzle", "guitar", "castle", "planet"]
+level_3 = ["developer", "algorithm", "xylophone", "cryptography", "jazz"]
 
 NUM_CHANCE = 10
 
+left_try = 0
+
+checked_letters = []
+
+def print_welcome_screen():
+     # 1. הודעת פתיחה מהצ'אט
+    width = 50
+    print("=" * width)
+    print("W E L C O M E   T O".center(width))
+    print("H A N G M A N   G A M E".center(width))
+    print("=" * width)
+    print("\n") 
+    hangman_art = r"""
+           _________
+          |/        |
+          |        (_)
+          |        \|/
+          |         |
+          |        / \
+          |
+         _|_
+    """
+
+    for line in hangman_art.split('\n'):
+        print(" " * 12 + line)
+        
+    print("\n") 
+
+    
+    print("-" * width)
+    print("Created by: SHMUEL".center(width))
+    print("Have Fun & Good Luck!".center(width))
+    print("-" * width)
+    print("\n") 
+
+def get_name_and_level():
+    user_name = input("please enter your name: ")
+    is_level = False
+    while not is_level:
+        user_level = input(f"please choose level" 
+                        f"1 (easy) "
+                        f"2 (medium) "
+                        f"3 (hard)")
+        if user_level == "1" or user_level == "2" or user_level == "3":
+            is_level = True
+            break
+        else:
+            print("there is a problem. your answer have to or '1' or '2' or '3' " )
+    return [user_name,user_level]
+
+
+def get_word(level):
+    list_from_level = globals()[f"level_{level}"]
+    current_word = random.choice(list_from_level)
+    return current_word
+
+def is_proper_input(char):
+    pass
+
+def add_correct_letter():
+    pass
+
+def show_current_mode():
+    pass
+
+def check_for_over_game():
+    pass
+
 def main_menu():
-    print()
+   print_welcome_screen()
+   name_and_level = get_name_and_level()
+
+
+
+
+def start_game():
+    pass
+
+print(get_word("1"))
